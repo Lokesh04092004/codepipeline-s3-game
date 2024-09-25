12473c6d8b533a8fe623ad5037d66d1837e84ac6
Lokesh04092004 <dllokesh97@gmail.com>
2024-09-25 20:59:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const cactus = document.getElementById('cactus');
 const scoreDisplay = document.getElementById('score');
 
 let score = 0;
-let isJumping = false;
+let isJumping = true;
 let gameOver = false;
 
 // Jump function
