3c6b38be44636dd18c645c6fe32ca3fe421f5725
Lokesh04092004 <dllokesh97@gmail.com>
2024-09-25 20:40:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,118 +1,38 @@
-const canvas = document.getElementById("pong");
-const context = canvas.getContext("2d");
-
-const paddleWidth = 10;
-const paddleHeight = 100;
-const ballSize = 10;
-
-let player = {
-    x: 0,
-    y: canvas.height / 2 - paddleHeight / 2,
-    width: paddleWidth,
-    height: paddleHeight,
-    color: "#fff",
-    score: 0
-};
-
-let computer = {
-    x: canvas.width - paddleWidth,
-    y: canvas.height / 2 - paddleHeight / 2,
-    width: paddleWidth,
-    height: paddleHeight,
-    color: "#fff",
-    score: 0
-};
-
-let ball = {
-    x: canvas.width / 2,
-    y: canvas.height / 2,
-    radius: ballSize,
-    speed: 5,
-    velocityX: 5,
-    velocityY: 5,
-    color: "#fff"
-};
-
-function drawRect(x, y, w, h, color) {
-    context.fillStyle = color;
-    context.fillRect(x, y, w, h);
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #f0f0f0;
 }
 
-function drawArc(x, y, r, color) {
-    context.fillStyle = color;
-    context.beginPath();
-    context.arc(x, y, r, 0, Math.PI * 2, false);
-    context.closePath();
-    context.fill();
+.board {
+    width: 600px;
+    height: 600px;
+    border: 10px solid #8B4513;
+    position: relative;
+    background-color: #e0b285;
 }
 
-function draw() {
-    drawRect(0, 0, canvas.width, canvas.height, "#222");
-    drawRect(player.x, player.y, player.width, player.height, player.color);
-    drawRect(computer.x, computer.y, computer.width, computer.height, computer.color);
-    drawArc(ball.x, ball.y, ball.radius, ball.color);
+.piece {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    position: absolute;
 }
 
-function update() {
-    ball.x += ball.velocityX;
-    ball.y += ball.velocityY;
-
-    // Ball collision with top and bottom walls
-    if (ball.y + ball.radius > canvas.height || ball.y - ball.radius < 0) {
-        ball.velocityY = -ball.velocityY;
-    }
-
-    // Ball collision with paddles
-    if (ball.x - ball.radius < player.x + player.width && 
-        ball.y > player.y && 
-        ball.y < player.y + player.height) {
-        ball.velocityX = -ball.velocityX;
-    }
-
-    if (ball.x + ball.radius > computer.x && 
-        ball.y > computer.y && 
-        ball.y < computer.y + computer.height) {
-        ball.velocityX = -ball.velocityX;
-    }
-
-    // Update computer paddle position
-    if (ball.y > computer.y + computer.height / 2) {
-        computer.y += 4;
-    } else {
-        computer.y -= 4;
-    }
-
-    // Reset ball if it goes off the left or right side
-    if (ball.x - ball.radius < 0) {
-        computer.score++;
-        resetBall();
-    } else if (ball.x + ball.radius > canvas.width) {
-        player.score++;
-        resetBall();
-    }
+.black {
+    background-color: black;
 }
 
-function resetBall() {
-    ball.x = canvas.width / 2;
-    ball.y = canvas.height / 2;
-    ball.velocityX = -ball.velocityX;
+.white {
+    background-color: white;
 }
 
-function gameLoop() {
-    draw();
-    update();
-    requestAnimationFrame(gameLoop);
+.queen {
+    background-color: red;
 }
 
-// Control player paddle with mouse
-canvas.addEventListener("mousemove", (event) => {
-    const rect = canvas.getBoundingClientRect();
-    player.y = event.clientY - rect.top - player.height / 2;
-
-    // Prevent paddle from going out of bounds
-    if (player.y < 0) player.y = 0;
-    if (player.y + player.height > canvas.height) player.y = canvas.height - player.height;
-});
-
-// Start the game loop
-gameLoop();
+.striker {
+    background-color: blue;
+}
