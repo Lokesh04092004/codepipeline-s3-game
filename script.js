0d4796164d22ee13eabf09e2ab40ac6b0fbbba37
Lokesh04092004 <dllokesh97@gmail.com>
2024-09-25 20:34:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,77 +1,118 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const grid = document.querySelector('#game-board');
-    const startButton = document.getElementById('start-game');
-    let cardsChosen = [];
-    let cardsChosenId = [];
-    let cardsWon = [];
+const canvas = document.getElementById("pong");
+const context = canvas.getContext("2d");
 
-    const cardArray = [
-        { name: 'card1', img: 'images/distracted.png' },
-        { name: 'card1', img: 'images/distracted.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
-        { name: 'card5', img: 'images/success.png' },
-        { name: 'card5', img: 'images/success.png' },
-        // ...add more pairs as needed
-    ];
+const paddleWidth = 10;
+const paddleHeight = 100;
+const ballSize = 10;
 
-    function shuffle(array) {
-        array.sort(() => 0.5 - Math.random());
+let player = {
+    x: 0,
+    y: canvas.height / 2 - paddleHeight / 2,
+    width: paddleWidth,
+    height: paddleHeight,
+    color: "#fff",
+    score: 0
+};
+
+let computer = {
+    x: canvas.width - paddleWidth,
+    y: canvas.height / 2 - paddleHeight / 2,
+    width: paddleWidth,
+    height: paddleHeight,
+    color: "#fff",
+    score: 0
+};
+
+let ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    radius: ballSize,
+    speed: 5,
+    velocityX: 5,
+    velocityY: 5,
+    color: "#fff"
+};
+
+function drawRect(x, y, w, h, color) {
+    context.fillStyle = color;
+    context.fillRect(x, y, w, h);
+}
+
+function drawArc(x, y, r, color) {
+    context.fillStyle = color;
+    context.beginPath();
+    context.arc(x, y, r, 0, Math.PI * 2, false);
+    context.closePath();
+    context.fill();
+}
+
+function draw() {
+    drawRect(0, 0, canvas.width, canvas.height, "#222");
+    drawRect(player.x, player.y, player.width, player.height, player.color);
+    drawRect(computer.x, computer.y, computer.width, computer.height, computer.color);
+    drawArc(ball.x, ball.y, ball.radius, ball.color);
+}
+
+function update() {
+    ball.x += ball.velocityX;
+    ball.y += ball.velocityY;
+
+    // Ball collision with top and bottom walls
+    if (ball.y + ball.radius > canvas.height || ball.y - ball.radius < 0) {
+        ball.velocityY = -ball.velocityY;
     }
 
-    function createBoard() {
-        shuffle(cardArray);
-        grid.innerHTML = '';
-        cardsWon = [];
-
-        for (let i = 0; i < cardArray.length; i++) {
-            const card = document.createElement('img');
-            card.setAttribute('src', 'images/blank.png');
-            card.setAttribute('data-id', i);
-            card.addEventListener('click', flipCard);
-            grid.appendChild(card);
-        }
+    // Ball collision with paddles
+    if (ball.x - ball.radius < player.x + player.width && 
+        ball.y > player.y && 
+        ball.y < player.y + player.height) {
+        ball.velocityX = -ball.velocityX;
     }
 
-    function flipCard() {
-        let cardId = this.getAttribute('data-id');
-        if (!cardsChosenId.includes(cardId)) {
-            cardsChosen.push(cardArray[cardId].name);
-            cardsChosenId.push(cardId);
-            this.setAttribute('src', cardArray[cardId].img);
-            if (cardsChosen.length === 2) {
-                setTimeout(checkForMatch, 500);
-            }
-        }
+    if (ball.x + ball.radius > computer.x && 
+        ball.y > computer.y && 
+        ball.y < computer.y + computer.height) {
+        ball.velocityX = -ball.velocityX;
     }
 
-    function checkForMatch() {
-        const cards = document.querySelectorAll('#game-board img');
-        const firstCardId = cardsChosenId[0];
-        const secondCardId = cardsChosenId[1];
-
-        if (cardsChosen[0] === cardsChosen[1] && firstCardId !== secondCardId) {
-            cards[firstCardId].style.visibility = 'hidden';
-            cards[secondCardId].style.visibility = 'hidden';
-            cards[firstCardId].removeEventListener('click', flipCard);
-            cards[secondCardId].removeEventListener('click', flipCard);
-            cardsWon.push(cardsChosen);
-        } else {
-            cards[firstCardId].setAttribute('src', 'images/blank.png');
-            cards[secondCardId].setAttribute('src', 'images/blank.png');
-        }
-
-        cardsChosen = [];
-        cardsChosenId = [];
-
-        if (cardsWon.length === cardArray.length / 2) {
-            alert('Congratulations! You found them all!');
-        }
+    // Update computer paddle position
+    if (ball.y > computer.y + computer.height / 2) {
+        computer.y += 4;
+    } else {
+        computer.y -= 4;
     }
 
-    startButton.addEventListener('click', createBoard);
+    // Reset ball if it goes off the left or right side
+    if (ball.x - ball.radius < 0) {
+        computer.score++;
+        resetBall();
+    } else if (ball.x + ball.radius > canvas.width) {
+        player.score++;
+        resetBall();
+    }
+}
+
+function resetBall() {
+    ball.x = canvas.width / 2;
+    ball.y = canvas.height / 2;
+    ball.velocityX = -ball.velocityX;
+}
+
+function gameLoop() {
+    draw();
+    update();
+    requestAnimationFrame(gameLoop);
+}
+
+// Control player paddle with mouse
+canvas.addEventListener("mousemove", (event) => {
+    const rect = canvas.getBoundingClientRect();
+    player.y = event.clientY - rect.top - player.height / 2;
+
+    // Prevent paddle from going out of bounds
+    if (player.y < 0) player.y = 0;
+    if (player.y + player.height > canvas.height) player.y = canvas.height - player.height;
 });
+
+// Start the game loop
+gameLoop();
