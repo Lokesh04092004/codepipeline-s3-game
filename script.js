1390b9123594e35aaa5a6a488318a20db517f874
Lokesh04092004 <dllokesh97@gmail.com>
2024-10-13 20:23:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,22 @@
-.game-board {
-    width: 500px;
-    height: 500px;
-    border: 1px solid black;
-    position: relative;
+const dice = document.querySelector('.dice');
+const players = document.querySelectorAll('.player');
+const pieces = document.querySelectorAll('.pieces');
+
+let currentPlayer = 0;
+let isRolling = false;
+
+function rollDice() {
+    if (!isRolling) {
+        isRolling = true;
+        const randomNumber = Math.floor(Math.random() * 6) + 1;
+        dice.textContent = randomNumber;
+
+        setTimeout(() => {
+            isRolling = false;
+            // Implement game logic based on the rolled number
+            // Update player positions, check for wins, etc.
+        }, 1000);
+    }
 }
 
-.player-home {
-    width: 100px;
-    height: 100px;
-    border: 1px solid black;
-    position: absolute;
-}
-
-/* Position player homes appropriately */
-
-.center {
-    width: 50px;
-    height: 50px;
-    border: 1px solid black;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-}
-
-.path {
-    /* Define the path using CSS or SVG */
-}
-
-.player-info {
-    display: flex;
-    justify-content: space-between;
-}
-
-.player {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
-
-.pieces {
-    display: flex;
-}
-
-.dice-container {
-    text-align: center;
-}
-
-.dice {
-    width: 50px;
-    height: 50px;
-    border: 1px solid black;
-    font-size: 24px;
-    text-align: center;
-}
+dice.addEventListener('click', rollDice);
